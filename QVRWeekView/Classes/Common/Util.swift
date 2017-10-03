ccebbea19e323d4bf39999fe95f608435f55ef8d
Ruslan Musagitov <ruslan.musagitov@gmail.com>
2017-10-03 18:05:53 +0600

    two number of lines set for the day label

--- QVRWeekView/Classes/Common/Util.swift
+++ QVRWeekView/Classes/Common/Util.swift
@@ -11,24 +11,24 @@ import Foundation
  Util struct provides static utility methods.
  */
 struct Util {
-
+    
     // Function returns a dayLabel UILabel with the correct size and position according to given indexPath.
     static func makeDayLabel(withIndexPath indexPath: IndexPath) -> TopBarViewContainer {
-
+        
         // Make as daylabel
         let frame = Util.generateDayLabelFrame(forIndex: indexPath)
         let view = TopBarViewContainer(frame: frame)
         return view
     }
-
+    
     /**
      Function returns true if given event from dayDate can not be found in the given eventStore,
      or if the event found in the eventStore with same id is different (has changed)
-    */
+     */
     static func isEvent(_ event: EventData, fromDay dayDate: DayDate, notInOrHasChanged eventStore: [DayDate: [String: EventData]]) -> Bool {
         return (eventStore[dayDate] == nil) || (eventStore[dayDate]![event.id] == nil) || (eventStore[dayDate]![event.id]! != event)
     }
-
+    
     /**
      Function returns true if given event from dayDate can not be found in the given eventStore,
      or if the event found in the eventStore with same id is different (has changed)
@@ -36,7 +36,7 @@ struct Util {
     static func isEvent(_ event: EventData, fromDay dayDate: DayDate, notInOrHasChanged eventStore: [DayDate: [EventData]]) -> Bool {
         return (eventStore[dayDate] == nil) || (!eventStore[dayDate]!.contains(event))
     }
-
+    
     // Function generates a frame for a day label with given index path.
     static func generateDayViewFrame(forIndex indexPath: IndexPath) -> CGRect {
         return generateDayLabelFrame(forIndex:indexPath)
@@ -47,7 +47,7 @@ struct Util {
         let row = CGFloat(indexPath.row)
         return CGRect(x: row*(LayoutVariables.totalDayViewCellWidth), y: 0, width: LayoutVariables.dayViewCellWidth, height: LayoutVariables.defaultTopBarHeight)
     }
-
+    
     /**
      Function will analyse the valid strings given from the dayDate object and determines which string will fit into the given
      label. Function will also check for font resizing if neccessary and will return the new font size if it is different to the
@@ -58,7 +58,7 @@ struct Util {
         let labelWidth = label.frame.width
         var possibleText = dayDate.getString(forMode: FontVariables.dayLabelTextMode) as NSString
         var textSize = possibleText.size(attributes: [NSFontAttributeName: currentFont])
-
+        
         label.text = possibleText as String
         if textSize.width > labelWidth && FontVariables.dayLabelTextMode != .small {
             possibleText = dayDate.defaultString as NSString
@@ -70,15 +70,15 @@ struct Util {
             else {
                 let scale = (labelWidth / textSize.width)
                 var newFont = currentFont.withSize(floor(currentFont.pointSize*scale))
-
+                
                 while possibleText.size(attributes: [NSFontAttributeName: newFont]).width > labelWidth && newFont.pointSize > FontVariables.dayLabelMinimumFontSize {
                     newFont = newFont.withSize(newFont.pointSize-0.25)
                 }
-
+                
                 if newFont.pointSize < FontVariables.dayLabelMinimumFontSize {
                     newFont = newFont.withSize(FontVariables.dayLabelMinimumFontSize)
                 }
-
+                
                 label.font = newFont
                 if possibleText.size(attributes: [NSFontAttributeName: newFont]).width > labelWidth {
                     label.text = dayDate.smallString
@@ -88,7 +88,7 @@ struct Util {
                     label.text = possibleText as String
                     FontVariables.dayLabelTextMode = .normal
                 }
-
+                
                 if newFont.pointSize < FontVariables.dayLabelCurrentFont.pointSize {
                     label.font = newFont
                     return newFont.pointSize
@@ -97,12 +97,12 @@ struct Util {
         }
         return nil
     }
-
+    
     // Method resets the day label text mode back to zero.
     static func resetDayLabelTextMode() {
         FontVariables.dayLabelTextMode = .large
     }
-
+    
     /**
      Functions generates a frame for an all day event according to the indexPath and
      the count (= how many'th all day event frame in current day) and the max (= how many all day events in current day.
@@ -115,20 +115,20 @@ struct Util {
                       width: width,
                       height: LayoutVariables.allDayEventHeight)
     }
-
+    
     static func getSize(ofString string: String, withFont font: UIFont, inFrame frame: CGRect) -> CGRect {
         let text = NSAttributedString(string: string, attributes: [NSFontAttributeName: font])
         return text.boundingRect(with: CGSize(width: frame.width, height: CGFloat.infinity), options: .usesLineFragmentOrigin, context: nil)
     }
-
+    
 }
 
 // Util extension for FontVariables.
 extension FontVariables {
-
+    
     // Day label text mode determines which format the day labels will be displayed in. 0 is the longest, 1 is smaller, 2 is smallest format.
     fileprivate(set) static var dayLabelTextMode: TextMode = .large
-
+    
 }
 
 protocol TopBarViewContainerProtocol : class {
@@ -145,6 +145,8 @@ class TopBarViewContainer: UIView {
     func update(withDate date: DayDate) {
         dayDate = date
         dayLabel.font = FontVariables.dayLabelCurrentFont
+        dayLabel.numberOfLines = 2
+        dayLabel.minimumScaleFactor = 0.25
         dayLabel.textColor = date == DayDate.today ? FontVariables.dayLabelTodayTextColor : FontVariables.dayLabelTextColor
         button.isHidden = date.hasPassed() && !date.isToday()
     }
@@ -178,7 +180,7 @@ class TopBarViewContainer: UIView {
         button.backgroundColor = UIColor.clear
         button.setImage(UIImage(named: "cross-mark"), for: .normal)
         addSubview(button)
-    
+        
     }
     
     override func layoutSubviews() {
@@ -186,10 +188,11 @@ class TopBarViewContainer: UIView {
         var labelRect = frame
         labelRect.origin.x = 0
         labelRect.origin.y = 0
-        labelRect.size.height = frame.size.height / 2
+        labelRect.size.height = frame.size.height / 1.5
         dayLabel.frame = labelRect
         
-        labelRect.origin.y = frame.size.height / 2
+        labelRect.origin.y = labelRect.size.height
+        labelRect.size.height = (frame.size.height - labelRect.origin.y) * 0.9
         button.frame = labelRect
     }
     
